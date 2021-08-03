fad63d4a88be0f2709a2631169ae0535c84a6e24
sourabh151 <jhamnanisourabh2@gmail.com>
2021-08-03 13:41:00 +0530

    first commit

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.mycompany.myapp;
 
 import android.app.*;
 import android.os.*;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +12,12 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	}
+    public void displayToastMsg(View v) {
+        toastMsg("Hello how are you today!!");
+    }
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
